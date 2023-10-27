2ecd5ea536853d1b81c995bce082e38926bed728
Matthew Hanson <mhanson.nflk@gmail.com>
2023-10-27 02:56:21 +0100

    Functional first pass

--- app.py
+++ app.py
@@ -1,46 +1,88 @@
 import streamlit as st
 import logging
-import LLM
+from utils import process_pdf
+from langchain.llms.gpt4all import GPT4All
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
 
 # This file mostly covers UI via streamlit and functions involving displayed output
 # Keep all management of the LLM model in the LLM.py file
+
+# page config
+st.set_page_config(page_title="PDF Questioner", page_icon="📚", layout="wide")
 
 # debug configration
 level = logging.DEBUG  # sets level of shown logs
 fmt = "[%(levelname)s] %(asctime)s - %(message)s"  # log output
 logging.basicConfig(level=level, format=fmt)  # configure module
 
+# template initiation
+template = """
+You are a recruiting agent attempting to get your candidate hired, and are answering questions from the hiring manager.
+Try to answer the question that highlights the positives of your candidate.
+You cannot lie, but you can omit information.
+
+The question is: {question}
+The context is: {context}
+
+output you answer in a human readable format. Don't tab or indent your answer.
+
+"""
+
+
+@st.cache_resource
+def load_model():
+    return GPT4All(model="mistral-7b-openorca.Q4_0.gguf", max_tokens=1000, streaming=True)  # type: ignore
+
+
 # session state initialisers
 if "history" not in st.session_state:
     st.session_state.history = [{"user": "ai", "body": "Hello, I am the AI, please ask me a question!"}]
-if "loaded_model" not in st.session_state:
-    st.session_state.model = LLM.PdfQuestioner()
+if "vector" not in st.session_state:
+    st.session_state.vector = None
+if "model" not in st.session_state:
+    st.session_state.model = load_model()
 
 # Streamlit Main UI
-st.set_page_config(page_title="PDF Questioner", page_icon="📚", layout="wide")
 st.title(body="PDF Questioner")
-st.write("Please load a PDF or saved vector before asking questions")
+st.markdown("Please load a PDF or saved vector before asking questions")
 st.warning("Intial loading may take a while, as it has to download the model")
 
 # PDF Sidebar
 with st.sidebar:
     st.title("Data Management")
     st.subheader("Loading PDF")
-    pdf_file = st.file_uploader(label="Upload PDF", type="pdf")
+    pdf_file = st.file_uploader(label="Upload PDF", type="pdf", accept_multiple_files=True)
     if st.button(label="Submit PDF", key="UploadPDF"):
-        pass
+        logging.debug(pdf_file)
+        with st.spinner(text="Loading PDFs"):
+            st.session_state.vector = process_pdf(pdf_file)
+        logging.debug(f"Vector loaded from PDF: {st.session_state.vector}")
 
     st.subheader("Load Saved Vectors")
-    st.write("This is where the Vectors dialog will go")
+    st.markdown("This is where the Vectors dialog will go")
 
 # Main Body
 chat = st.container()
-if st.session_state.model.loaded:
-    for message in st.session_state.history:
-        with st.chat_message(name=message["user"]):
-            st.write(message["body"])
+for message in st.session_state.history:
+    with st.chat_message(name=message["user"]):
+        st.markdown(message["body"])
 
-    if prompt := st.chat_input("", key="question"):
-        with st.chat_message("user"):
-            st.markdown(prompt)
-            st.session_state.history.append({"user": "user", "body": prompt})
+if prompt := st.chat_input("", key="question"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+        st.session_state.history.append({"user": "user", "body": prompt})  # type: ignore
+
+    if st.session_state.vector is None:
+        st.error("Please load a PDF or saved vector before asking questions")
+        st.stop()
+
+    chain = (
+        {"question": RunnablePassthrough(), "context": st.session_state.vector.as_retriever()}
+        | PromptTemplate.from_template(template)
+        | st.session_state.model
+    )
+    # with st.spinner(text="Generating Answer"):
+    with st.chat_message("ai"):
+        for message in chain.stream(prompt):
+            st.markdown(message)
---
+++ utils.py
@@ -0,0 +1,50 @@
+## This is the code for the LLM model itself.
+## all GUI code should be managed in app.py
+## The model is a class that can then be used within via app.py
+
+# Imports
+import logging
+from PyPDF2 import PdfReader
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings.gpt4all import GPT4AllEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+
+
+# debug configration
+level = logging.DEBUG  # sets level of shown logs
+fmt = "[%(levelname)s] %(asctime)s - %(message)s"  # log output
+logging.basicConfig(level=level, format=fmt)  # configure module
+
+
+def __load_pdf(files):
+    # takes a list of pdf files and returns a list of pages
+    # logging.debug(f"Loading PDF: {files}")
+    pages = []
+    for pdf in files:
+        reader = PdfReader(pdf)
+        for page in reader.pages:
+            pages.append(page.extract_text())
+    # logging.debug(f"Loaded PDF: {pages[0]}")
+    return pages
+
+
+def __chunk_text(text):
+    # takes a string and returns a list of strings
+    # logging.debug(f"Chunking text: {text}")
+    splitter = CharacterTextSplitter(separator="\n\n", chunk_size=1000, chunk_overlap=200)
+    chunks = splitter.create_documents(text)
+    return chunks
+
+
+def __vectorise_chunks(chunks):
+    # logging.debug(f"Vectorising chunks: {chunks[0]}")
+    embedding = GPT4AllEmbeddings(model_name="mistral-7b-instruct-v0.1.Q4_0.gguf", client=None)
+    vector = FAISS.from_documents(chunks, embedding=embedding)
+    return vector
+
+
+def process_pdf(files):
+    pages = __load_pdf(files)
+    chunks = __chunk_text(pages)
+    vector = __vectorise_chunks(chunks)
+    return vector
