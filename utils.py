e20cddcc2208162639cd2da4ff8c1db2d54c810b
Matthew Hanson <mhanson.nflk@gmail.com>
2023-10-31 04:14:08 +0000

    quick styling adjustment (Constants)

--- utils.py
+++ utils.py
@@ -11,13 +11,13 @@ from langchain.text_splitter import CharacterTextSplitter
 
 
 # debug configration
-level = logging.DEBUG  # sets level of shown logs
-fmt = "[%(levelname)s] %(asctime)s - %(message)s"  # log output
-logging.basicConfig(level=level, format=fmt)  # configure module
+LEVEL = logging.DEBUG  # sets level of shown logs
+FMT = "[%(levelname)s] %(asctime)s - %(message)s"  # log output
+logging.basicConfig(level=LEVEL, format=FMT)  # configure module
 
 
+# takes a list of pdf files and returns a list of pages
 def __load_pdf(files):
-    # takes a list of pdf files and returns a list of pages
     # logging.debug(f"Loading PDF: {files}")
     pages = []
     for pdf in files:
@@ -28,6 +28,7 @@ def __load_pdf(files):
     return pages
 
 
+# Chunks text into 400 character chunks with 100 character overlap
 def __chunk_text(text):
     # takes a string and returns a list of strings
     # logging.debug(f"Chunking text: {text}")
@@ -37,6 +38,7 @@ def __chunk_text(text):
     return chunks
 
 
+# places chunks into vector db
 def __vectorise_chunks(chunks):
     # logging.debug(f"Vectorising chunks: {chunks[0]}")
     embedding = GPT4AllEmbeddings(model_name="mistral-7b-instruct-v0.1.Q4_0.gguf", client=None)  # type: ignore
@@ -44,6 +46,7 @@ def __vectorise_chunks(chunks):
     return vector
 
 
+# main processing function to call
 def process_pdf(files):
     pages = __load_pdf(files)
     chunks = __chunk_text(pages)
